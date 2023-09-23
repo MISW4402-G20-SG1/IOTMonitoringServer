9fbabef076353cf20fb4351d1481076ee9b2ca0a
William Mendez <wifemendez28@gmail.com>
2023-09-23 12:34:23 -0500

    ips

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "18.207.208.33"]
+ALLOWED_HOSTS = ["localhost", "34.236.143.243"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "34.201.244.29",  # Dirección IP de la base de datos
+        "HOST": "34.207.209.18",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.83.206.45"
+MQTT_HOST = "54.221.57.169"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
